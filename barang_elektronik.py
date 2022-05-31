5753a910bd68dfda340735b8b7920003da594464
nurulfauzh <nrulfauzh@gmail.com>
2022-05-31 15:44:36 +0700

    Nurul 1

---
+++ barang_elektronik.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class BarangElektronik(ABC) :
+
+    @abstractmethod
+    def beroperasi(self) :
+        pass
+
+    @abstractmethod
+    def berhenti(self) :
+        pass
+    
